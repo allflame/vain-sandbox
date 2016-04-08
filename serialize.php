db51810c63cd9e6531fa96cebbf080d710070dff
Andrey Dembitskiy <andrey.dembitskiy@cosmonova.net>
2016-04-08 13:44:49 +0300

    Fix class names according to changes in vendor. Add new params to constructor DescriptorFactory.

--- serialize.php
+++ serialize.php
@@ -9,11 +9,14 @@ require __DIR__ . '/vendor/autoload.php';
 
 $basicModuleFactory = new \Vain\Expression\Module\Factory\ModuleFactory();
 $expressionFactory = new \Vain\Expression\Factory\ExpressionFactory();
-$humanParser = new \Vain\Expression\Parser\Human\HumanExpressionParser();
+$humanParser = new \Vain\Expression\Parser\Human\HumanParser();
 $moduleFactory = new \Vain\Sandbox\Data\Module\Factory\SandboxModuleFactory($basicModuleFactory);
 $moduleRepository = new \Vain\Expression\Module\Repository\ModuleRepository($moduleFactory);
-$descriptorFactory = new \Vain\Expression\Descriptor\Factory\DescriptorFactory($moduleRepository);
-$serializer = new \Vain\Expression\Serializer\ExpressionSerializer($expressionFactory, $descriptorFactory);
+$comparatorFactory = new \Vain\Comparator\Factory\ComparatorFactory();
+$comparatorRepository = new \Vain\Comparator\Repository\ComparatorRepository($comparatorFactory);
+$evaluator = new \Vain\Expression\Evaluator\Evaluator($comparatorRepository);
+$descriptorFactory = new \Vain\Expression\Descriptor\Factory\DescriptorFactory($moduleRepository, $evaluator);
+$serializer = new \Vain\Expression\Serializer\Serializer($expressionFactory, $descriptorFactory);
 
 
 $expression = new \Vain\Expression\Comparison\Less\LessExpression(
@@ -25,5 +28,5 @@ printf('Before serialization:%s%s%s', PHP_EOL, $expression->parse($humanParser),
 $serializedExpression = $serializer->serializeExpression($expression);
 printf('Serialized presentation:%s%s%s', PHP_EOL, json_encode($serializedExpression), PHP_EOL);
 $unserializedExpression = $serializer->unserializeExpression($serializedExpression);
-$humanParser = new \Vain\Expression\Parser\Human\HumanExpressionParser();
+$humanParser = new \Vain\Expression\Parser\Human\HumanParser();
 printf('After unserialization:%s%s%s', PHP_EOL, $unserializedExpression->parse($humanParser), PHP_EOL);
